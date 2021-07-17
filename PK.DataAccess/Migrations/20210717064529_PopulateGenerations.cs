1a3069b9b204a7ca9dacd054f56deddfa91f21ff
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-07-17 04:05:32 -0300

    [PK-5] Add Generations data

---
+++ PK.DataAccess/Migrations/20210717064529_PopulateGenerations.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateGenerations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Generations",
+                column: "Name",
+                values: new string[]
+                {
+                    "Generation I",
+                    "Generation II",
+                    "Generation III",
+                    "Generation IV",
+                    "Generation V",
+                    "Generation VI",
+                    "Generation VII",
+                    "Generation VIII"
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("Generations",
+                keyColumn: "Name",
+                keyValues: new string[]
+                {
+                    "Generation I",
+                    "Generation II",
+                    "Generation III",
+                    "Generation IV",
+                    "Generation V",
+                    "Generation VI",
+                    "Generation VII",
+                    "Generation VIII"
+                });
+        }
+    }
+}
