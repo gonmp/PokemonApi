ffc8831b28f9bc6740a49dbfcb72ece2cb8cc33e
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-07-31 10:49:31 -0300

    Add version service and controller

---
+++ PK.Core/Services/Versions/VersionsService.cs
@@ -0,0 +1,14 @@
+﻿using PK.DataAccess;
+
+namespace PK.Core.Services.Versions
+{
+    public class VersionsService
+    {
+        private readonly DataAccessContext _context;
+
+        public VersionsService(DataAccessContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ PK.Web/Controllers/VersionsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PK.Web.Controllers
+{
+    [ApiController]
+    [Route("{language}/[Controller]")]
+    public class VersionsController : ControllerBase
+    {
+    }
+}
