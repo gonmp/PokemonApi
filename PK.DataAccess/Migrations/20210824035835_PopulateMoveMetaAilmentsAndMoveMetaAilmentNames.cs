94c3415c730fbeac826d28b470ccfa38f0fc9ac3
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-24 19:59:01 -0300

    [PK-29] Populate MoveMetaAilments and MoveMetaAilmentNames

---
+++ PK.DataAccess/Migrations/20210824035835_PopulateMoveMetaAilmentsAndMoveMetaAilmentNames.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMoveMetaAilmentsAndMoveMetaAilmentNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("MoveMetaAilments", "MoveMetaAilments.csv");
+            migrationBuilder.InsertDataFromCsv("MoveMetaAilmentNames", "MoveMetaAilmentNames.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveMetaAilmentNames");
+            migrationBuilder.DeleteAllDataFromTable("MoveMetaAilments");
+        }
+    }
+}
