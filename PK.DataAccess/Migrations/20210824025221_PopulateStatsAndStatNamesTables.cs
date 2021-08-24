d42d5e3d87dc67494d8590948d5326c083ab7dfe
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-24 00:01:17 -0300

    [PK-25] Populate Stats and Stats Names

---
+++ PK.DataAccess/Migrations/20210824025221_PopulateStatsAndStatNamesTables.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateStatsAndStatNamesTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("Stats", "Stats.csv");
+            migrationBuilder.InsertDataFromCsv("StatNames", "StatNames.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("StatNames");
+            migrationBuilder.DeleteAllDataFromTable("Stats");
+        }
+    }
+}
