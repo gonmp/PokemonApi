518108a11ae61081ac70217f8d2a04cf63e55fea
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-24 00:52:46 -0300

    [PK-28] Populate MoveMetaCategories and MoveMetaCategoryProses

---
+++ PK.DataAccess/Migrations/20210824034632_PopulateMoveMetaCategoriesAndMoveMetaCategoryProses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMoveMetaCategoriesAndMoveMetaCategoryProses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("MoveMetaCategories", "MoveMetaCategories.csv");
+            migrationBuilder.InsertDataFromCsv("MoveMetaCategoryProses", "MoveMetaCategoryProses.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveMetaCategoryProses");
+            migrationBuilder.DeleteAllDataFromTable("MoveMetaCategories");
+        }
+    }
+}
