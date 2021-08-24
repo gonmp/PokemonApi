128c81b9cbe43e982ee08e9abd3f24231d4e01e8
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-24 00:30:55 -0300

    [PK-27] Populate MoveTargets and MoveTargetProses

---
+++ PK.DataAccess/Migrations/20210824031832_PopulateMoveTargetsAndMoveTargetProses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMoveTargetsAndMoveTargetProses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("MoveTargets", "MoveTargets.csv");
+            migrationBuilder.InsertDataFromCsvWithTabSeparator("MoveTargetProses", "MoveTargetProses.txt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveTargetProses");
+            migrationBuilder.DeleteAllDataFromTable("MoveTargets");
+        }
+    }
+}
