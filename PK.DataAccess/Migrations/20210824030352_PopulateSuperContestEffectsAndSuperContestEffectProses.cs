9332075eef37320ce5fa6449b3e4a476d837f0e2
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-24 00:07:53 -0300

    [PK-26] Populate SuperContestEffects and SuperContestEffectProses

---
+++ PK.DataAccess/Migrations/20210824030352_PopulateSuperContestEffectsAndSuperContestEffectProses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateSuperContestEffectsAndSuperContestEffectProses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("SuperContestEffects", "SuperContestEffects.csv");
+            migrationBuilder.InsertDataFromCsv("SuperContestEffectProses", "SuperContestEffectProses.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("SuperContestEffectProses");
+            migrationBuilder.DeleteAllDataFromTable("SuperContestEffects");
+        }
+    }
+}
