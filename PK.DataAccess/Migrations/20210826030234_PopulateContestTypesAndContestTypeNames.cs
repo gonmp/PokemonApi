527b2ef9175b3861bf48a302e70145e26af9ed23
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-26 00:05:44 -0300

    [PK-34] Populate ContestTypes and ContestTypeNames

---
+++ PK.DataAccess/Migrations/20210826030234_PopulateContestTypesAndContestTypeNames.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateContestTypesAndContestTypeNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("ContestTypes", "ContestTypes.csv");
+            migrationBuilder.InsertDataFromCsv("ContestTypeNames", "ContestTypeNames.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("ContestTypeNames");
+            migrationBuilder.DeleteAllDataFromTable("ContestTypes");
+        }
+    }
+}
