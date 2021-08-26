f9095e5791ce0510cb2c2e27c70f3d9461f74e0d
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-26 00:48:58 -0300

    [PK-38] Populate Moves and MoveNames

---
+++ PK.DataAccess/Migrations/20210826034655_PopulateMovesAndMoveNames.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMovesAndMoveNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("Moves", "Moves.csv");
+            migrationBuilder.InsertDataFromCsv("MoveNames", "MoveNames.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveNames");
+            migrationBuilder.DeleteAllDataFromTable("Moves");
+        }
+    }
+}
