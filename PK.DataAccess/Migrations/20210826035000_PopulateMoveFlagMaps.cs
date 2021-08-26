e476cf03fdd270bb0cb10075d98bdd3908ced29b
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-26 00:52:08 -0300

    [PK-39] Populate MoveFlagMaps

---
+++ PK.DataAccess/Migrations/20210826035000_PopulateMoveFlagMaps.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMoveFlagMaps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("MoveFlagMaps", "MoveFlagMaps.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveFlagMaps");
+        }
+    }
+}
