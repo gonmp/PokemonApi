769c2462fc9129523d12704d69479793ae94aba8
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-26 00:27:11 -0300

    [PK-36] Populate ContestEffects and ContestEffectProses

---
+++ PK.DataAccess/Migrations/20210826032336_PopulateContestEffectsAndContestEffectProses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateContestEffectsAndContestEffectProses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("ContestEffects", "ContestEffects.csv");
+            migrationBuilder.InsertDataFromCsvWithTabSeparator("ContestEffectProse", "ContestEffectProses.txt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("ContestEffectProse");
+            migrationBuilder.DeleteAllDataFromTable("ContestEffects");
+        }
+    }
+}
