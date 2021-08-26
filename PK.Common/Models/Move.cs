1207ad959763eec87a72eef9c89e8a7c10771ee8
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-26 00:46:21 -0300

    [PK-37] Alter Moves make nullable fields in Move

--- PK.Common/Models/Move.cs
+++ PK.Common/Models/Move.cs
@@ -13,9 +13,9 @@ namespace PK.Common.Models
         public Generation Generation { get; set; }
         public int TypeId { get; set; }
         public Type Type { get; set; }
-        public int Power { get; set; }
-        public int PowerPoints { get; set; }
-        public int Accuracy { get; set; }
+        public int? Power { get; set; }
+        public int? PowerPoints { get; set; }
+        public int? Accuracy { get; set; }
         public int Priority { get; set; }
         public int TargetId { get; set; }
         public MoveTarget Target { get; set; }
@@ -23,12 +23,12 @@ namespace PK.Common.Models
         public MoveDamageClass DamageClass { get; set; }
         public int EffectId { get; set; }
         public MoveEffect Effect { get; set; }
-        public int EffectChance { get; set; }
-        public int ContestTypeId { get; set; }
+        public int? EffectChance { get; set; }
+        public int? ContestTypeId { get; set; }
         public ContestType ContestType { get; set; }
-        public int ContestEffectId { get; set; }
+        public int? ContestEffectId { get; set; }
         public ContestEffect ContestEffect { get; set; }
-        public int SuperContestEffectId { get; set; }
+        public int? SuperContestEffectId { get; set; }
         public SuperContestEffect SuperContestEffect { get; set; }
     }
 }
---
+++ PK.DataAccess/Migrations/20210826034453_AlterMovesMakeNullableFieldsInMove.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class AlterMovesMakeNullableFieldsInMove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SuperContestEffectId",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PowerPoints",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Power",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EffectChance",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContestTypeId",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContestEffectId",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Accuracy",
+                table: "Moves",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SuperContestEffectId",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PowerPoints",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Power",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EffectChance",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContestTypeId",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ContestEffectId",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Accuracy",
+                table: "Moves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
