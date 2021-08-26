741605490674fa981b412ee566e7053b42368f26
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-08-25 23:54:29 -0300

    [PK-33] Populate MoveBattleStyles and MoveBattleStyleProses

---
+++ PK.DataAccess/Migrations/20210826024908_PopulateMoveBattleStylesAndMoveBattleStyleProses.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateMoveBattleStylesAndMoveBattleStyleProses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("MoveBattleStyles", "MoveBattleStyles.csv");
+            migrationBuilder.InsertDataFromCsv("MoveBattleStyleProses", "MoveBattleStyleProses.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("MoveBattleStyleProses");
+            migrationBuilder.DeleteAllDataFromTable("MoveBattleStyles");
+        }
+    }
+}
