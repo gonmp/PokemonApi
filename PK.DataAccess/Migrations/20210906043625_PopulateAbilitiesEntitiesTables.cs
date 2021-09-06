b68a9884286b2dcf172aa2e97ebee4fd05e1c810
Gonzalo Perelstein <gmperelstein@gmail.com>
2021-09-06 01:38:59 -0300

    [PK-47] Populate abilities entities tables

---
+++ PK.DataAccess/Migrations/20210906043625_PopulateAbilitiesEntitiesTables.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using PK.DataAccess.Extensions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PK.DataAccess.Migrations
+{
+    public partial class PopulateAbilitiesEntitiesTables : Migration
+    {
+        [ExcludeFromCodeCoverage]
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertDataFromCsv("Abilities", "Abilities.csv");
+            migrationBuilder.InsertDataFromCsvWithTabSeparator("AbilityFlavorTexts", "AbilityFlavorTexts.txt");
+            migrationBuilder.InsertDataFromCsv("AbilityNames", "AbilityNames.csv");
+            migrationBuilder.InsertDataWithBreakLines("AbilityProses",
+                new string[]
+                {
+                    "Id",
+                    "AbilityId",
+                    "LocalLanguageId",
+                    "ShortEffect",
+                    "Effect"
+                },
+                "AbilityProses.csv");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteAllDataFromTable("AbilityProses");
+            migrationBuilder.DeleteAllDataFromTable("AbilityNames");
+            migrationBuilder.DeleteAllDataFromTable("AbilityFlavorTexts");
+            migrationBuilder.DeleteAllDataFromTable("Abilities");
+        }
+    }
+}
